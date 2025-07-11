01f6dca66e555f3bf34a20302023b2cd33c81875
areisde <alexandre@unki.ch>
2025-07-12 00:30:59 +0200

    testing simple again

--- function_app.py
+++ function_app.py
@@ -1,7 +1,4 @@
 import azure.functions as func
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from api import crawl
 #from api import ingest
 #from api import retrieve
