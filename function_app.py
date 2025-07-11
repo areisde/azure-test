d95f13eb2451d1789117961ddb76754bb625a6db
areisde <alexandre@unki.ch>
2025-07-12 00:25:33 +0200

    Adding root to the filepath avaiable

--- function_app.py
+++ function_app.py
@@ -1,4 +1,7 @@
 import azure.functions as func
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from api import crawl
 #from api import ingest
 #from api import retrieve
